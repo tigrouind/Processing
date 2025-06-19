baf6468953586fbfda0caa4dba357921273f63ff
Tigrou Ind <tigrou.ind@gmail.com>
2025-06-19 18:45:50 +0200

    Initial commit

---
+++ Tools/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+ * Created by SharpDevelop.
+ * User: erensonnet
+ * Date: 24.06.2025
+ * Time: 12:23
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace Tools
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
